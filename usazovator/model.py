22d6822ef36fb6fe222a644c155f8029c215434a
Jan Dvořák <jan.dvorak@singularita.net>
2017-05-19 15:34:34 +0200

    Clean up the code base
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

---
+++ usazovator/model.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3 -tt
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+
+from requests import get, Session
+from requests.auth import HTTPBasicAuth
+from zeep.transports import Transport
+from zeep import Client
+
+
+__all__ = ['Usazovator', 'Wifinator', 'Asset']
+
+
+class Usazovator:
+    def __init__(self, asset, wifinator, capacity):
+        # Clients for the two services we gather our data from.
+        self.wifinator = wifinator
+        self.asset = asset
+        self.capacity = capacity
+
+    def get_user_count(self):
+        # Get current WiFi association statistics for all zones.
+        stations = self.wifinator.get_stations()
+
+        # Discard the zones we are not interested in.
+        for name in list(stations):
+            if name not in self.capacity:
+                del stations[name]
+
+        # Add up the numbers from all those zones.
+        wifi_count = sum(stations.values())
+
+        # Obtain current total number of users in the building.
+        user_count = self.asset.get_user_count()
+
+        # Calculate the ratio between users and their wireless devices.
+        ratio = (user_count / wifi_count) if wifi_count > 0 else 0
+
+        # We want to retain configured zone ordering.
+        zones = OrderedDict()
+
+        # Multiple wireless device counts with the ratio to convert
+        # them into approximate user counts.
+        for name in self.capacity:
+            zones[name] = round(stations.get(name, 0) * ratio)
+
+        # Return both total users and zone occupancy.
+        return user_count, zones
+
+
+class Asset:
+    """Client for the ASSET SOAP interface."""
+
+    def __init__(self, wsdl, user, password, zone_id):
+        session = Session()
+        session.auth = HTTPBasicAuth(user, password)
+        self.client = Client(wsdl=wsdl, transport=Transport(session=session))
+        self.zone_id = int(zone_id)
+
+    def get_user_count(self):
+        zones = self.client.service.GetZoneUserCount(self.zone_id)
+
+        for zone in zones:
+            if zone['ZoneId'] == self.zone_id:
+                return zone['UserCount']
+
+        return 0
+
+
+class Wifinator:
+    """Client for the Wifinator public interface."""
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_stations(self):
+        try:
+            stations = get(self.url, verify=False)
+            return stations.json()
+        except OSError:
+            return {}
+
+
+# vim:set sw=4 ts=4 et:
--- usazovator/site.py
+++ usazovator/site.py
@@ -24,146 +24,47 @@ __all__ = ['make_site']
 
 import colorsys
 
-def make_site(manager, access_model, debug=False, auth=False, cors=False):
+def make_site(model, debug=False):
     """
     Create the WSGI site object using Flask.
     """
 
-    app = Flask('.'.join(__name__.split('.')[:-1]))
+    app = Flask(__name__)
     app.secret_key = urandom(16)
     app.debug = debug
 
+    @app.template_global()
+    def get_color(value, maximum):
+        hue = 0.30 * min(value / maximum, 1.0)
+        g, r, b = colorsys.hsv_to_rgb(hue, 1, 1)
 
-    @app.context_processor
-    def utility_processor():
-        def get_color(value, maximum):
-            hue = (min(value / maximum, 1.0) * 0.30)
-            g,r,b = colorsys.hsv_to_rgb(hue, 1, 1)
-            return '%02x%02x%02x' % (int(r*255), int(g*255), int(b*255))
-        return dict(get_color=get_color)
-
-    def has_privilege(privilege):
-        roles = request.headers.get('X-Roles', '')
-
-        if not roles or '(null)' == roles:
-            roles = ['impotent']
-        else:
-            roles = findall(r'\w+', roles)
-
-        return access_model.have_privilege(privilege, roles)
-
-    def pass_user_info(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            uid = request.headers.get('X-User-Id', '0')
-            username = request.headers.get('X-Full-Name', 'Someone')
-
-            kwargs.update({
-                'uid': int(uid),
-                'username': username.encode('latin1').decode('utf8'),
-            })
-
-            return fn(*args, **kwargs)
-        return wrapper
-
-    def pass_depth(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            kwargs['depth'] = int(request.args.get('depth', '0'))
-            return fn(*args, **kwargs)
-        return wrapper
-
-    def authorized_only(privilege='user'):
-        def make_wrapper(fn):
-            @wraps(fn)
-            def wrapper(*args, **kwargs):
-                if not has_privilege(privilege):
-                    raise Forbidden('RBAC Forbidden')
-
-                return fn(*args, **kwargs)
-
-            return wrapper
-        return make_wrapper
+        return '%02x%02x%02x' % (int(r * 255), int(g * 255), int(b * 255))
 
     @app.errorhandler(Forbidden.code)
     def unauthorized(e):
         return render_template('forbidden.html')
 
-    @app.errorhandler(IntegrityError)
-    def integrity_error(error):
-        log.msg('IntegrityError: {}'.format(error))
-
-        response = jsonify({
-            'error': 'integrity',
-            'message': str(error.orig),
-        })
-
-        response.status_code = 400
-        return response
-
-    @app.errorhandler(SQLAlchemyError)
-    def sqlalchemy_error(error):
-        log.msg('SQLAlchemyError: {}'.format(error))
-
-        response = jsonify({
-            'error': 'database',
-            'message': str(error.orig),
-        })
-
-        response.status_code = 400
-        return response
-
-    @app.errorhandler(KeyError)
-    def key_error(error):
-        log.msg('KeyError: {}'.format(error))
-
-        response = jsonify({
-            'error': 'key',
-            'message': str(error),
-        })
-
-        response.status_code = 404
-        return response
-
-    @app.errorhandler(ValueError)
-    def value_error(error):
-        log.msg('ValueError: {}'.format(error))
-
-        response = jsonify({
-            'error': 'value',
-            'message': str(error),
-        })
-
-        response.status_code = 400
-        return response
-
     @app.route('/')
     def index():
-        nonlocal has_privilege
-        user_count = manager.get_user_count()
-        total = manager.asset.get_user_count()
-        capacity = manager.capacity
+        total, user_count = model.get_user_count()
+        capacity = model.capacity
         return render_template('index.html', **locals())
 
     @app.route('/horizontal')
     def horizontal():
-        nonlocal has_privilege
-        user_count = manager.get_user_count()
-        total = manager.asset.get_user_count()
-        capacity = manager.capacity
+        total, user_count = model.get_user_count()
+        capacity = model.capacity
         return render_template('horizontal.html', **locals())
 
     @app.route('/svg')
     def svg():
-        nonlocal has_privilege
-        user_count = manager.get_user_count()
-        total = manager.asset.get_user_count()
-        capacity = manager.capacity
+        total, user_count = model.get_user_count()
+        capacity = model.capacity
         return render_template('svg.html', **locals())
 
     @app.route('/json')
     def json():
-        user_count = manager.get_user_count()
+        total, user_count = model.get_user_count()
         return jsonify(user_count)
 
 
