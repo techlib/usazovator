0005bef4c0b903c2d5f6ffab8ba44423a059c149
Jan Dvořák <jan.dvorak@singularita.net>
2017-05-19 16:13:42 +0200

    Remove unneeded dependencies
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- usazovator/site.py
+++ usazovator/site.py
@@ -3,26 +3,14 @@
 
 from twisted.python import log
 
-from werkzeug.exceptions import Forbidden, NotFound
-from flask import Flask, Response, request, render_template, jsonify
-
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-from sqlalchemy import desc
-
-from os.path import dirname, join
-from functools import wraps
-from base64 import b64decode
-from time import time
 from os import urandom
-from re import findall
-from io import BytesIO
-from imghdr import what
+from colorsys import hsv_to_rgb
+
+from flask import Flask, Response, request, render_template, jsonify
 
 
 __all__ = ['make_site']
 
-
-import colorsys
 
 def make_site(model, debug=False):
     """
@@ -36,7 +24,7 @@ def make_site(model, debug=False):
     @app.template_global()
     def get_color(value, maximum):
         hue = 0.30 * min(value / maximum, 1.0)
-        g, r, b = colorsys.hsv_to_rgb(hue, 1, 1)
+        g, r, b = hsv_to_rgb(hue, 1, 1)
 
         return '%02x%02x%02x' % (int(r * 255), int(g * 255), int(b * 255))
 
