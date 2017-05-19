2dfe8b07a52f6bd61c517a311dbac54d66be0996
Jan Dvořák <jan.dvorak@singularita.net>
2017-05-19 11:27:25 +0200

    Use our common setup.py style
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3 -tt
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import *
 
-setup(
-    name = 'usazovator',
-    version = '1',
-    author = 'NTK',
-    description = ('physical security management'),
-    license = 'MIT',
-    keywords = 'physical security management',
-    url = 'http://github.com/techlib/usazovator',
-    packages=[
-        'usazovator',
-    ],
-    classifiers=[
+def read_requires(path=None):
+    if path is None:
+        path = join(dirname(__file__), 'requirements.txt')
+        print(path)
+
+    with open(path) as fp:
+        return [l.strip() for l in fp.readlines()]
+
+setup(**{
+    'name': 'usazovator',
+    'version': '0.1.0',
+    'author': 'NTK',
+    'description': ('NTK occupancy display'),
+    'license': 'MIT',
+    'keywords': 'building occupancy',
+    'url': 'http://github.com/techlib/usazovator/',
+    'include_package_data': True,
+    'packages': find_packages(),
+    'classifiers': [
         'License :: OSI Approved :: MIT License',
     ],
-    scripts=['bin/usazovator']
-)
+    'entry_points': '''
+        [console_scripts]
+        usazovatorctl=usazovator.bin.usazovatorctl:cli
+        usazovatord=usazovator.bin.usazovatord:cli
+    ''',
+    'install_requires': read_requires(),
+})
 
 
 # vim:set sw=4 ts=4 et:
