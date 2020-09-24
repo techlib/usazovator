5f83b37a175fba00241955e9542954e2255d4dfe
ZtF <o.koch@zerusnet.com>
2020-09-24 16:49:33 +0200

    Added a little more color and better refresh using Unpoly.js

--- usazovator/static/js/usazovator.js
+++ usazovator/static/js/usazovator.js
@@ -1,9 +1,19 @@
+function getColor(value) {
+  //value from 0 to 1
+  var hue = ((1 - value) * 120).toString(10);
+  return ["hsl(", hue, ",100%,50%)"].join("");
+}
+
 function startRefresh() {
-  $.get(document.location, function(data) {
-    $(document.body).html(data);
-  });
+  up.reload('html').then(() => {
+    var max = 500;
+    var current = parseInt($('#counter').text());
+    $('#counter').css('color', getColor(current/max));
+    setTimeout(startRefresh, 1000);
+  })
 }
 
 $(function() {
-  setTimeout(startRefresh, 5000);
+  setTimeout(startRefresh, 1000);
 });
+
