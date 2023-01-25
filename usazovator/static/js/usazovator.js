3a3244063a76430272a0f5e9cee9fde6bbc53c55
Ondrej Koch <o.koch@zerusnet.com>
2023-01-25 14:15:11 +0100

    Better better blue

--- usazovator/static/js/usazovator.js
+++ usazovator/static/js/usazovator.js
@@ -1,14 +1,13 @@
 function getColor(value) {
-    var percentage = value/1200
     hue0 = 120
     hue1 = 0
-    var hue = (percentage * (hue1 - hue0)) + hue0;
+    var hue = (value * (hue1 - hue0)) + hue0;
     return 'hsl(' + hue + ', 100%, 50%)';
 }
 
 function startRefresh() {
   up.reload('#page').then(() => {
-    var max = 500;
+    var max = 1200;
     var current = parseInt($('#counter').text());
     $('#counter').css('color', getColor(current/max));
   })
